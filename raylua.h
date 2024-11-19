6aec1863f21fc3cf1ba2c2625fa27c0e10de3667
George Watson <gigolo@hotmail.co.uk>
2024-11-19 11:45:56 +0000

    assed a header for the one function lol

---
+++ raylua.h
@@ -0,0 +1,14 @@
+//
+//  raylua.h
+//  wwwww
+//
+//  Created by George Watson on 17/11/2024.
+//
+
+#ifndef RAYLUA_HEADER
+#define RAYLUA_HEADER
+#include "minilua.h"
+
+void initialize_raylua(lua_State *L);
+
+#endif // RAYLUA_HEADER
